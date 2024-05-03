5d334ce2677e4c9218cd72b8e0f03ea3ca5bfbd2
yash1314 <yashkeshari79@gmail.com>
2024-05-03 13:25:55 +0530

    exception handling

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ button = st.button('Analyze')
 
 
 if input_text and button:
-    try :    
-        output = model(input_text)
-    except Exception as e:
-        pass
-    else: 
-        final_output = output_processing(output)
-        for key, value in final_output.items():
-            st.success(f"**{key}** : {value}")    
+    # try :    
+    #     output = model(input_text)
+    # except Exception as e:
+    #     pass
+    # else: 
+    final_output = output_processing(output)
+    for key, value in final_output.items():
+        st.success(f"**{key}** : {value}")    
